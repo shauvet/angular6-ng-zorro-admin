c36095453bfde296d610f5730f02f926b19233a1
yangxiaowei <wangzhe23@gmail.com>
2018-06-08 21:52:39 +0800

    update user service

--- src/app/bz-modules/user/user.module.ts
+++ src/app/bz-modules/user/user.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { userRoutes } from './user.routes';
 import { UserComponent } from './user/user.component';
+import { UserService } from './user.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(userRoutes)
   ],
-  declarations: [UserComponent]
+  declarations: [UserComponent],
+  providers: [
+    UserService
+  ]
 })
 export class UserModule { }
--- src/app/bz-modules/user/user/user.component.ts
+++ src/app/bz-modules/user/user/user.component.ts
@@ -8,7 +8,9 @@ import { UserService } from '../user.service';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+    console.log('in user.');
+   }
 
   ngOnInit() {
     this.userService.getUsers().subscribe(users => {
