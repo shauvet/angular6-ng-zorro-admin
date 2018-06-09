acb596f7a144ca9a91b1e38b841e3fd8c1927a5e
yangxiaowei <wangzhe23@gmail.com>
2018-06-09 22:43:03 +0800

    update

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators
-} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     NgZorroAntdModule.forRoot()
   ],
   declarations: [LoginComponent]
