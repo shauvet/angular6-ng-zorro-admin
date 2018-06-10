cef2e8f6b9fde005ed89cdabb8c74fd83b0d827b
yangxiaowei <wangzhe23@gmail.com>
2018-06-10 08:55:39 +0800

    update user data

--- src/app/bz-modules/user/user/user.component.ts
+++ src/app/bz-modules/user/user/user.component.ts
@@ -7,34 +7,15 @@ import { UserService } from '../user.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  dataSet = [
-    {
-      key    : '1',
-      name   : 'John Brown',
-      age    : 32,
-      address: 'New York No. 1 Lake Park'
-    },
-    {
-      key    : '2',
-      name   : 'Jim Green',
-      age    : 42,
-      address: 'London No. 1 Lake Park'
-    },
-    {
-      key    : '3',
-      name   : 'Joe Black',
-      age    : 32,
-      address: 'Sidney No. 1 Lake Park'
-    }
-  ];
+  dataSet = [];
 
   constructor(private userService: UserService) {
-    console.log('in user.');
    }
 
   ngOnInit() {
     this.userService.getUsers().subscribe(users => {
       console.log(users);
+      this.dataSet = users;
     });
   }
 
